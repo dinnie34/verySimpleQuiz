9b142867e36aa241ef362c1bd86f240eac7ebbde
dinnie34 <brodey.black@gmail.com>
2021-09-08 18:24:36 -0400

    cleanup

--- verysimplequiz.py
+++ verysimplequiz.py
@@ -1,38 +1,42 @@
-print('Hello, you smell bad!')
-
-ans = input('Ready pal? (yes/no): ')
+##### Variables
 score = 0
 total_q = 4
 
-if ans.lower() == 'yes':
-    ans = input('1. What is 2 and 2 added together? ')
-    if ans == '4':
+##### Main
+print('Hello, you smell bad!')
+
+ans = input('Ready pal? (yes/no): ').lower()
+if ans in ['yes']:
+    ans = input('1.in What is 2 and 2 added together? ')
+    if ans in ['4']:
         score += 1
-        print('Correct')
+        print("Correct!")
     else:
-        print('Incorrect')
+        print("Incorrect!")
 
 
     ans = input('2. What is the capital of Ohio? ').lower()
-    if ans == 'columbus':
+    if ans in ['columbus']:
         score += 1
         print('Correct')
     else:
         print('Incorrect')
 
 
-    ans = input('3. Is Tyler gay? ')
-    if ans.lower() == 'yes':
+    ans = input('3. Is Tyler gay? ').lower()
+    if ans in ['yes']:
         score += 1
         print('Correct')
     else:
         print('y ou shou ld not h ave don e that')
 
 
-    ans = input('4. Did you answer yes on the last question? ')
-    if ans.lower() == 'yes':
+    ans = input('4. Did you answer yes on the last question? ').lower()
+    if ans in ['yes', 'y', 'yeah']:
         score += 1
         print('Correct')
+    elif ans in ["maybe"]:
+        print("gfgdfh")
     else:
         print('b ett er no t sl eep a lon e')
 
@@ -42,6 +46,7 @@ if ans.lower() == 'yes':
 
     print("Mark: ", mark)
 print('I will be watching :)')
+input()
 
 
 
